ba2829803fbdb697e8a4a63ed7cf1385bc866125
Steven Quan <63191202+wizardap@users.noreply.github.com>
2023-04-19 10:46:44 +0700

    Update

--- AdvProg_L6-Painter/painter.h
+++ AdvProg_L6-Painter/painter.h
@@ -7,8 +7,7 @@
 //Mac
 #include <SDL2/SDL.h>
 //Windows
-// #include <SDL.h>
-
+//#include <SDL.h>
 const SDL_Color CYAN_COLOR = {0, 255, 255};
 const SDL_Color BLUE_COLOR = {0, 0, 255};
 const SDL_Color ORANGE_COLOR = {255, 165, 0};
@@ -19,47 +18,74 @@ const SDL_Color RED_COLOR = {255, 0, 0};
 const SDL_Color WHITE_COLOR = {255, 255, 255};
 const SDL_Color BLACK_COLOR = {0, 0, 0};
 const SDL_Color GREEN_COLOR = {0, 128, 0};
-
 const SDL_Color DEFAULT_COLOR = BLACK_COLOR;
 
 /***
-   An Implementation of Code.org Painter 
+   An Implementation of Code.org Painter
 ***/
 class Painter
 {
-    private:
-        int x, y;
-        double angle;
-        int width, height;
-        SDL_Color color;
-        SDL_Renderer *renderer;
+private:
+    int x, y;
+    double angle;
+    int width, height;
+    SDL_Color color;
+    SDL_Renderer *renderer;
 
-    public:
+public:
     Painter();
-	Painter(SDL_Window* window, SDL_Renderer *renderer);
-        void setColor(SDL_Color color);
-        SDL_Color getColor() { return color; }
-        void setPosition(int x, int y) { this->x = x; this->y = y; }
-        int getX() { return x; }
-        int getY() { return y; }
-        SDL_Renderer* getRenderer() { return this->renderer; }
-        void setAngle(double angle) { this->angle = angle; }
-        double getAngle() { return angle; }
-        int getWidth() { return width; }
-        int getHeight() { return height; }
-        void clearWithBgColor(SDL_Color bgColor);
+    Painter(SDL_Window* window, SDL_Renderer *renderer);
+    void setColor(SDL_Color color);
+    SDL_Color getColor()
+    {
+        return color;
+    }
+    void setPosition(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    int getX()
+    {
+        return x;
+    }
+    int getY()
+    {
+        return y;
+    }
+    SDL_Renderer* getRenderer()
+    {
+        return this->renderer;
+    }
+    void setAngle(double angle)
+    {
+        this->angle = angle;
+    }
+    double getAngle()
+    {
+        return angle;
+    }
+    int getWidth()
+    {
+        return width;
+    }
+    int getHeight()
+    {
+        return height;
+    }
+    void clearWithBgColor(SDL_Color bgColor);
 
-        void moveForward(int numPixel);
-        void moveBackward(int numPixel);
-        void turnLeft(double degree);
-        void turnRight(double degree);
-        void jumpForward(int numPixel);
-        void jumpBackward(int numPixel);
-        void createCircle(int radius);
-        void randomColor();
+    void moveForward(int numPixel);
+    void moveBackward(int numPixel);
+    void turnLeft(double degree);
+    void turnRight(double degree);
+    void jumpForward(int numPixel);
+    void jumpBackward(int numPixel);
+    void createCircle(int radius);
+    void randomColor();
 
-        void createParallelogram(int size);
-        void createSquare(int size);
+    void createParallelogram(int size);
+    void createSquare(int size);
 };
 
 #endif
